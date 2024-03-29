16bea7e483622af28c5723d7d12616f4ae8f59b9
JUMP_2SMOOTH2\2smoothuser <2smoothuser01@asiaplus.co.th>
2024-03-29 11:07:59 +0700

    move to auth service

--- src/app/api/auth/permission/route.ts
+++ src/app/api/auth/permission/route.ts
@@ -1,65 +1,19 @@
 import { Constants } from "@/constants/constants";
+import { AuthService, Permission } from "@/services/auth-service/auth-service";
 import { VaultService } from "@/services/vault-service/vault-service";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
-    const vaultService = new VaultService(Constants.VaultUrl, Constants.VaultUsername, Constants.VaultPassword);
-    const token = await vaultService.getAuthenJWTToken();
+    const authService = new AuthService("");
 
-    const pathUrl = `${VaultService.vaultInfo?.data.data.baseUrlAuthen}/api/UserPermission`
-
-    const employeeID = cookies().get('user')?.value || ''
-    console.log(`employeeID`, employeeID )
-    //console.log(`cookies().get('user')?.value`, _req.cookies.get('user')?.value )
-    //console.log(`cookies().getAll()`, _req.cookies.getAll() )
-
-    const payload: PermissionRequest = {
-        appCode: Constants.AppCode,
-        menuCode: "",
-        menuAction: "",
-        userId: employeeID
-    }
-
-    const queryParams = Object.keys(payload).map((key) => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(payload[key as keyof PermissionRequest ] );
-    }).join('&');
-
-    console.log(pathUrl + `?${queryParams}`)
-
-    const permissionResp = await fetch(pathUrl + `?${queryParams}`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${token}`
-        },
-        cache: 'no-cache'
-    })
-
-    const permissionJson = await permissionResp.json();
-    const filterViewPermission = filterViewPermision(permissionJson);
+    const permissionJson = await authService.getPermission();
+    const filterViewPermission = filterViewPermision(permissionJson.data);
     return NextResponse.json( filterViewPermission );
 }
 
-const filterViewPermision = (permission: PermissionResponse): Permission[] => {
+const filterViewPermision = (permission: Permission[]): Permission[] => {
     const action = 'view'
-    return permission.data.filter( (permission : Permission) => (permission.menuAction || '').includes(action)) || []
+    return permission.filter( (permission : Permission) => (permission.menuAction || '').includes(action)) || []
 }
 
-interface PermissionRequest{
-    appCode: string
-    menuCode: string
-    menuAction: string
-    userId: string
-}
-
-interface PermissionResponse{
-    data: Permission[]
-    status: string
-    message: string
-}
-
-export interface Permission{
-    menuCode: string
-    menuName: string
-    menuAction: string
-}
--- src/services/auth-service/auth-service.ts
+++ src/services/auth-service/auth-service.ts
@@ -1,6 +1,7 @@
 import { Constants } from "@/constants/constants";
 import { PortalService } from "../portal-service/portal-service";
 import { VaultService } from "../vault-service/vault-service";
+import { cookies } from "next/headers";
 
 export class AuthService{
 
@@ -33,4 +34,61 @@ export class AuthService{
         });
         console.log(`response`, await response.json())
     }
+
+    async getPermission(): Promise<PermissionResponse>{
+        const vaultService = new VaultService(Constants.VaultUrl, Constants.VaultUsername, Constants.VaultPassword);
+        const token = await vaultService.getAuthenJWTToken();
+
+        const pathUrl = `${VaultService.vaultInfo?.data.data.baseUrlAuthen}/api/UserPermission`
+
+        const employeeID = cookies().get('user')?.value || ''
+        console.log(`employeeID`, employeeID )
+        if( !employeeID ) return {
+            data: [],
+            message: '',
+            status: 'error'
+        } as PermissionResponse;
+
+        const payload: PermissionRequest = {
+            appCode: Constants.AppCode,
+            menuCode: "",
+            menuAction: "",
+            userId: employeeID
+        }
+    
+        const queryParams = Object.keys(payload).map((key) => {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(payload[key as keyof PermissionRequest ] );
+        }).join('&');
+    
+        console.log(pathUrl + `?${queryParams}`)
+    
+        const permissionResp = await fetch(pathUrl + `?${queryParams}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            cache: 'no-cache'
+        })
+        
+        return await permissionResp.json();
+    }
+}
+
+export interface PermissionRequest{
+    appCode: string
+    menuCode: string
+    menuAction: string
+    userId: string
+}
+
+export interface PermissionResponse{
+    data: Permission[]
+    status: string
+    message: string
+}
+
+export interface Permission{
+    menuCode: string
+    menuName: string
+    menuAction: string
 }
