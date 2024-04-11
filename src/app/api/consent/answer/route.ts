ab88aecc173787a6df96347775a5c99cf4c30bb2
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-11 16:54:10 +0700

    update service bundler, update api routes

---
+++ src/app/api/consent/answer/route.ts
@@ -0,0 +1,14 @@
+/**
+ *  API Route - Consent Answer
+ */
+
+import { NextRequest, NextResponse } from "next/server";
+import { services } from "@/services";
+
+export async function GET(req: NextRequest) {
+  const referenceId = req.nextUrl.searchParams.get('referenceId') || '';
+  const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
+  const apiKyc = await services.getKycApi();
+  const res = await apiKyc.getConsentApi().consentReferenceIdGetAnsweredGet({ referenceId, formId });
+  return NextResponse.json(res.data.data);
+}
