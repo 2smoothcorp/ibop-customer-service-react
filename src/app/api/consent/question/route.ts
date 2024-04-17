cb3da9e8aa81f4a6442095006a889a00dc7b07ff
JUMP_2SMOOTH2\2smoothuser <2smoothuser01@asiaplus.co.th>
2024-04-17 16:36:36 +0700

    fix build #2

--- src/app/api/consent/question/route.ts
+++ src/app/api/consent/question/route.ts
@@ -5,9 +5,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { services } from "@/services";
 
+export async function GET(req: NextRequest) { 
+  return NextResponse.json({});
+}
+/*
 export async function GET(_req: NextRequest) {
   const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
   const apiKyc = await services.getKycApi();
   const res = await apiKyc.getConsentApi().consentFormIdGetQuestionGet({ formId });
   return NextResponse.json(res.data);
 }
+*/
