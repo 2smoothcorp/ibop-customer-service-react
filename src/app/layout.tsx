c299e32f6af577e469cf8e210ca114cd21bcf512
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-18 18:02:22 +0700

    setup mui theme provider & its config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { mainTheme } from '@/libs/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import type { Metadata } from 'next';
+import './globals.css';
 
-export const metadata: Metadata = {
-  title: "iBOP - Customer Service",
-  description: "ASP iBOP - Customer Service",
-};
-
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   /*
   const UserDataSession = await cookies().get('user')?.value
   const headersList = headers();
-  const referer = headersList.get("referer");
+  const referer = headersList.get('referer');
   
   if( !UserDataSession && !(referer||'').includes('/login') ){
     redirect('/login')
@@ -22,10 +15,17 @@ export default async function RootLayout({
   */
 
   return (
-    <html lang="en">
+    <html lang={'en'}>
       <body>
-        {children}
+        <ThemeProvider theme={ mainTheme }>
+          { children }
+        </ThemeProvider>
       </body>
     </html>
   );
+}
+
+export const metadata: Metadata = {
+  title: "iBOP - Customer Service",
+  description: "ASP iBOP - Customer Service",
 }
---
+++ src/libs/theme.tsx
@@ -0,0 +1,62 @@
+/**
+ *  Theme - MUI Theme Builder
+ */
+
+import { createTheme } from '@mui/material/styles';
+
+export const mainTheme = createTheme({
+  direction: 'ltr',
+  palette: {
+    mode: 'light',
+    common: { black: '#000000', white: '#FFFFFF' },
+    contrastThreshold: 3,
+    divider: 'rgba(0, 0, 0, 0.25)',
+
+    primary: {
+      main: '#11285C',
+      light: '#6D96E4',
+      dark: '#0A2154',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      main: '#43AD9E',
+      light: '#75D3C3',
+      dark: '#2F9D8F',
+      contrastText: '#000000'
+    },
+    info: {
+      main: '#0EA5E9',
+      light: '#38BCF8',
+      dark: '#028AC7',
+      contrastText: '#000000'
+    },
+    success: {
+      main: '#10B981',
+      light: '#34D39E',
+      dark: '#059666',
+      contrastText: '#FFFFFF'
+    },
+    warning: {
+      main: '#F59E0B',
+      light: '#FBAB24',
+      dark: '#D98B06',
+      contrastText: '#000000'
+    },
+    error: {
+      main: '#EF4444',
+      light: '#F87171',
+      dark: '#DC2626',
+      contrastText: '#FFFFFF'
+    }
+  },
+
+  /** @ref https://stackoverflow.com/a/72078615 */
+  typography: {
+    fontFamily: `'DB Helvethaica', sans-serif`,
+    // fontSize: 14,
+    // htmlFontSize: 16
+  },
+
+  /** Custom Component Specific Theme here */
+  // components: {}
+});
