c630e2419ffcff29da5d3b2eab57bcf2a5a6b6b7
JUMP_2SMOOTH2\2smoothuser <2smoothuser01@asiaplus.co.th>
2024-04-19 10:08:04 +0700

    add try catch to fix api error on build

--- src/app/api/consent/question/route.ts
+++ src/app/api/consent/question/route.ts
@@ -6,8 +6,16 @@ import { services } from "@/services";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_req: NextRequest) {
-  const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
-  const apiKyc = await services.getKycApi();
-  const res = await apiKyc.getConsentApi().consentFormIdGetQuestionGet({ formId });
-  return NextResponse.json(res.data);
+  try{
+    const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
+    const apiKyc = await services.getKycApi();
+    const res = await apiKyc.getConsentApi().consentFormIdGetQuestionGet({ formId });
+    return NextResponse.json(res.data);
+  }catch(e){
+    return NextResponse.json({
+        message: "Error"
+      }, {
+        status: 500,
+      })
+  }
 }
