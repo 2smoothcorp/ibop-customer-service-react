ded10f8c10019b58ec43779fee6a5304ebac4064
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-19 10:35:43 +0700

    re-wrap api route try-catch

--- src/app/api/consent/answer/route.ts
+++ src/app/api/consent/answer/route.ts
@@ -6,9 +6,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { services } from "@/services";
 
 export async function GET(req: NextRequest) {
-  const corporateId = req.nextUrl.searchParams.get('corporateId') || '';
-  const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
-  const apiService = await services.getCustomerServiceApi();
-  const res = await apiService.getConsentApi().consentGetAnsweredCorparateIdGet({ corparateId: corporateId, formId });
-  return NextResponse.json(res.data);
+  try {
+    const corporateId = req.nextUrl.searchParams.get('corporateId') || '';
+    const formId = '28BFB1E6-C5ED-4D33-BB7D-99008A92258A';
+    const apiService = await services.getCustomerServiceApi();
+    const res = await apiService.getConsentApi().consentGetAnsweredCorparateIdGet({ corparateId: corporateId, formId });
+    return NextResponse.json(res.data);
+  }
+  catch(err: any) {
+    return NextResponse.json({ message: '[ERROR] /api/consent/answer', err }, { status: 500 });
+  }
 }
