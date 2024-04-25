c2bb47b1b42cceb31c7b31f07849822730daf22a
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-25 16:45:37 +0700

    add input-number - wrapping input and apply its style w/o other layout

---
+++ src/components/input-number/index.tsx
@@ -0,0 +1,43 @@
+/**
+ *  Component - Input Number
+ */
+
+'use client'
+
+import {
+  type ReactElement,
+  useEffect
+} from 'react';
+import { type NumericFormatProps, NumericFormat } from 'react-number-format';
+import type { UseFormRegister, FieldValues } from 'react-hook-form';
+
+import styles from './styles.module.css';
+
+export const InputNumber = (props: InputNumberProps): ReactElement => {
+  useEffect(() => {}, []);
+
+  const registerHookForm = () => {
+    const { name, register } = props;
+    if(!register) { return ({}); }
+    return register(name, { valueAsNumber: true });
+  }
+
+  return (
+    <NumericFormat
+      decimalScale={0}
+      decimalSeparator={'.'}
+      thousandSeparator={','}
+      inputMode={'numeric'}
+      { ...props }
+
+      className={[  styles['number-input'], '' ].join(' ')}
+
+      { ...registerHookForm() }
+    />
+  );
+}
+
+type InputNumberProps = NumericFormatProps & {
+  name: string;
+  register?: UseFormRegister<FieldValues>;
+}
