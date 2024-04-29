56fa343f1ea82c693e28be3499f024034463d1cb
JUMP_2SMOOTH2\2smoothuser <2smoothuser01@asiaplus.co.th>
2024-04-30 00:05:22 +0700

    add edif offline mode page

--- src/app/(app)/CustomerProfile/page.tsx
+++ src/app/(app)/CustomerProfile/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import SearchCustomerProfileListForm from "@/containers/CustomerProfile/index/SearchCustomerProfileListForm";
-import SearchCustomerProfileListTable from "@/containers/CustomerProfile/index/SearchCustomerProfileListTable";
-import {
-  AppBar,
-  Toolbar,
-  Typography
-} from "@mui/material";
-import { GridPaginationModel } from "@mui/x-data-grid";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { useFormState } from "react-dom";
-import { searchCustomerInfo } from "./customerProfile.action";
+import CustomerProfileContent from "./customer-profile-content";
 
-interface SearchParams {
+
+export interface SearchParams {
   corporateID?: string
   referenceID?: string
   fullName?: string
@@ -22,81 +12,15 @@ interface SearchParams {
   pageSize?: string | number
 }
 
-const CustomerInfoPage = ({
-  searchParams
-}: {
+export interface CustomerInfoPageProps {
   searchParams?: SearchParams
-}) => {
+  onClickWithEditEvent?: any //(coperateId: number) => void
+}
 
-  // console.log(`page`, searchParams?.pageIndex)
-
-  const router = useRouter();
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const [paginationModel, setPaginationModel] = useState<GridPaginationModel>({
-    pageSize: 10,
-    page: 1,
-  });
-
-  const [searchCustomerInfoState, searchCustomerInfoAction] = useFormState(searchCustomerInfo, {
-    data: undefined,
-    success: false,
-    error: undefined
-  })
-
-  const [isLoading, setIsLOading] = useState<boolean>(false)
-
-  const prepareBeforeSendAction = (formData: FormData) => {
-    setPaginationModel((prev) => ({
-      ...prev,
-      page: 1,
-    })
-    )
-  }
-
-
-  useEffect(() => {
-    if (!searchCustomerInfoState.success && searchCustomerInfoState?.error) {
-      alert(searchCustomerInfoState?.error)
-    }
-    setIsLOading(false)
-
-  }, [searchCustomerInfoState])
-
-  useEffect(() => {
-    if (formRef.current && paginationModel) {
-      setIsLOading(true)
-      const formData = new FormData(formRef.current);
-      formData.set('pageIndex', paginationModel?.page?.toString() || '');
-      formData.set('pageSize', paginationModel?.pageSize?.toString() || '');
-      //formRef.current && formRef.current?.requestSubmit();
-      searchCustomerInfoAction(formData);
-    }
-  }, [paginationModel])
-
-  return (
-    <>
-      <AppBar
-        sx={{ backgroundColor: '#1F346B' }}
-        className="bg-primary"
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div">
-            Customer Info list
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <form action={prepareBeforeSendAction} ref={formRef} >
-        <SearchCustomerProfileListForm />
-        <SearchCustomerProfileListTable
-          response={searchCustomerInfoState}
-          paginationModel={paginationModel}
-          setPaginationModel={setPaginationModel}
-          isLoading={isLoading}
-        />
-      </form>
-    </>
-  );
+const CustomerInfoPage = ({ searchParams, onClickWithEditEvent }: CustomerInfoPageProps) => {
+  return <>
+    <CustomerProfileContent />
+  </>
 };
 
 
