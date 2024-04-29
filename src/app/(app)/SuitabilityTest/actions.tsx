fd6d24c797bbf0baad637aa732a0ab0ce62a4138
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-29 17:10:19 +0700

    prepare suittest

---
+++ src/app/(app)/SuitabilityTest/actions.tsx
@@ -0,0 +1,52 @@
+/**
+ *  KYC CDD - List Action
+ */
+
+'use server'
+
+import { services } from '@/services';
+import type { KycGetAllOutputListDataResponse } from '@/services/rest-api/customer-service';
+
+export const search = async (input: ActionSearch.Input): Promise<ActionSearch.Output> => {
+  const {
+    corporateId,
+    referenceId,
+    fullname,
+    dateStart,
+    dateEnd,
+    page = 1,
+    pageSize = 10
+  } = input;
+
+  try {
+    const apiService = await services.getCustomerServiceApi();
+    // const result = await apiService.getSuitabilityTestApi().all({
+    //   corporateId, referenceId,
+    //   name: fullname,
+    //   startDate: dateStart,
+    //   endDate: dateEnd,
+    //   pageNumber: page,
+    //   pageSize
+    // });
+
+    const result: Array<any> = [];
+    return result;
+  }
+  catch(err) {
+    return ({ data: [] });
+  }
+}
+
+export module ActionSearch {
+  export interface Input {
+    corporateId?: string;
+    referenceId?: string;
+    fullname?: string;
+    dateStart?: Date;
+    dateEnd?: Date;
+    page?: number;
+    pageSize?: number;
+  }
+
+  export type Output = any;
+}
