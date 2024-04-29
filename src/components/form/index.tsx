b0f6bc8be47e6f99419287e3e2194d4490d18a51
Sarit Naraballobh <epzilon.s@gmail.com>
2024-04-29 17:11:49 +0700

    update form component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -6,9 +6,11 @@
 
 import {
   type ReactElement,
+  type FormEventHandler,
   useEffect
 } from 'react';
 import { Button, Grid } from '@mui/material';
+import type { UseFormRegister, FieldValues } from 'react-hook-form';
 
 import { InputText } from '@/components/input-text';
 import { InputNumber } from '@/components/input-number';
@@ -16,29 +18,28 @@ import { InputSelect } from '@/components/input-select';
 import { InputRadio } from '@/components/input-radio';
 import { InputCheckbox } from '@/components/input-checkbox';
 
-export const Form = (props: FormProps): ReactElement => {
+export const Form = <DynamicField extends {}>(props: FormProps<DynamicField>): ReactElement => {
   //
 
   useEffect(() => {}, []);
 
   const generateFields = () => {
-    const { fields, isEditing } = props;
+    const { fields, baseColSpan, isEditing } = props;
 
     return fields.map((item, idx) => {
       const {
         label,
         viewText,
-        colSpan = 6, labelCol = 6, viewCol = 6
+        colSpan = baseColSpan || 6, labelCol = 6, viewCol = 6
       } = item;
       return (
         <Grid container item spacing={2}
-          key={idx} xs={colSpan}
+          key={idx} xs={ colSpan }
           // className={'flex items-center gap-x-2'}
         >
           <Grid item
             xs={ labelCol }
-            className={['flex items-center', labelCol === 12 ? '' : 'flex justify-end'].join(' ')}
-            // style={{ paddingTop: 'unset' }}
+            className={[ 'flex items-center', /* 'mt-2.5', */ (labelCol === 12) ? '' : 'flex justify-end' ].join(' ')}
           >
             <strong>{ label }</strong>
           </Grid>
@@ -54,13 +55,14 @@ export const Form = (props: FormProps): ReactElement => {
   }
 
   const renderInput = (info: InputField): ReactElement => {
+    const { register } = props;
     const { inputCol, type, name, value, disabled } = info;
     switch(type) {
       case 'text': {
         const {} = info;
         return (
           <Grid item xs={ inputCol }>
-            <InputText name={ name } value={ value } disabled={ disabled } />
+            <InputText name={ name } value={ value } disabled={ disabled } register={ register } />
           </Grid>
         );
       }
@@ -68,7 +70,7 @@ export const Form = (props: FormProps): ReactElement => {
         const {} = info;
         return (
           <Grid item xs={ inputCol }>
-            <InputNumber name={ name } value={ value } readOnly={ disabled } />
+            <InputNumber name={ name } value={ value } readOnly={ disabled } register={ register } />
           </Grid>
         );
       }
@@ -76,7 +78,7 @@ export const Form = (props: FormProps): ReactElement => {
         const { options } = info;
         return (
           <Grid item xs={ inputCol }>
-            <InputSelect name={ name } options={ options } value={ value } disabled={ disabled } />
+            <InputSelect name={ name } options={ options } value={ value } disabled={ disabled } register={ register } />
           </Grid>
         );
       }
@@ -84,7 +86,7 @@ export const Form = (props: FormProps): ReactElement => {
         const { options } = info;
         return (
           <Grid item xs={ inputCol }>
-            <InputRadio row name={ name } options={ options } value={ value } />
+            <InputRadio row name={ name } options={ options } value={ value } register={ register } />
           </Grid>
         );
       }
@@ -92,15 +94,19 @@ export const Form = (props: FormProps): ReactElement => {
         const { options } = info;
         return (
           <Grid item xs={ inputCol }>
-            <InputCheckbox name={ name } options={ options } value={ value } disabled={ disabled } />
+            <InputCheckbox name={ name } options={ options } value={ value } disabled={ disabled } register={ register } />
           </Grid>
         );
       }
     }
+
+    return (
+      <Grid item xs={ inputCol }></Grid>
+    );
   }
 
   return (
-    <form action={ props.action }>
+    <form action={ props.action } onSubmit={ props.onSubmit }>
       <Grid container spacing={2}>
         { generateFields() }
 
@@ -111,7 +117,7 @@ export const Form = (props: FormProps): ReactElement => {
                 <div className={'flex items-center justify-center gap-x-4'}>
                   <Button type={'reset'}
                     variant={'contained'}
-                    className={'bg-neutral-200 hover:bg-neutral-200 hover:brightness-95 text-xl py-0 w-25 h-10 text-black'}
+                    className={'bg-neutral-200 hover:bg-neutral-200 hover:brightness-95 text-xl py-0 w-25 h-10'}
                   >
                     ย้อนกลับ
                   </Button>
@@ -128,13 +134,23 @@ export const Form = (props: FormProps): ReactElement => {
   );
 }
 
-interface FormProps {
+interface FormProps<DynamicField extends {}> {
   fields: Array<InputField>;
-  action?: (formData: FormData) => void;
   isEditing?: boolean;
+
+  /**
+   * Form field grid
+   * @defaut 6
+   */
+  baseColSpan?: ColumnSize;
+
+  register?: UseFormRegister<DynamicField>;
+  action?: (formData: FormData) => void;
+  onSubmit?: FormEventHandler<HTMLFormElement>;
 }
 
-type InputField = TextInputField
+type InputField = EmptySpaceField
+  | TextInputField
   | NumberInputField
   | SelectInputField
   | CheckboxInputField
@@ -148,35 +164,37 @@ interface BaseInputField {
    * Form field grid
    * @defaut 6
    */
-  colSpan?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  colSpan?: ColumnSize;
 
   /**
    * Column span for Label part
    * @defaut 6
    */
-  labelCol?: number;
+  labelCol?: ColumnSize;
 
   /**
    * Column span for Text part (view mode)
    * @default 6
    */
-  viewCol?: number;
+  viewCol?: ColumnSize;
 
   /**
    * Column span for input part (edit mode)
    * @default 6
    */
-  inputCol?: number;
+  inputCol?: ColumnSize;
 
-  type?: 'text' | 'number' | 'select' | 'checkbox' | 'radio';
+  type?: 'text' | 'number' | 'select' | 'checkbox' | 'radio' | 'space';
   name?: string;
   value?: string;
   disabled?: boolean;
 }
 
 interface WithInputOption { options: Array<{ label: string; value: string; disabled?: boolean; }>; }
+interface EmptySpaceField extends BaseInputField { type: 'space'; }
 interface TextInputField extends BaseInputField { type: 'text'; name: string; }
 interface NumberInputField extends BaseInputField { type: 'number'; name: string; }
 interface SelectInputField extends BaseInputField, WithInputOption { type: 'select'; name: string; }
 interface CheckboxInputField extends BaseInputField, WithInputOption { type: 'checkbox'; name: string; }
 interface RadioInputField extends BaseInputField, WithInputOption { type: 'radio'; name: string; }
+type ColumnSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
