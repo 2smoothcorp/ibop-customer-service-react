9baf0cfda7dc83db038cc3d1f7f835c33295d78b
Sarit Naraballobh <epzilon.s@gmail.com>
2024-05-03 11:48:35 +0700

    update type definition api

--- src/type/api.ts
+++ src/type/api.ts
@@ -1,7 +1,18 @@
+/**
+ *  Type Definition - API Extension
+ */
+
+import type { ComboBox } from '@/services/rest-api/customer-service';
+
 export interface ApiResponse<T>{
-    success?: boolean
-    message?: string
-    reason?: string
-    err?: any
-    data?: T
-  }
+  success?: boolean
+  message?: string
+  reason?: string
+  err?: any
+  data?: T
+}
+
+export type ComboBoxWrapperOption = ComboBox & {
+  label: string;
+  value: string;
+}
