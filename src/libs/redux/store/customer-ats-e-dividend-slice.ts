141f3e15043c8eabb2aea6ded89ba03d6cc85359
BUNDIT KWALAMTAN <bundit.kw@asiaplus.co.th>
2024-05-07 16:58:12 +0700

    add flow edit e-dividend

--- src/libs/redux/store/customer-ats-e-dividend-slice.ts
+++ src/libs/redux/store/customer-ats-e-dividend-slice.ts
@@ -3,15 +3,17 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 // Define a type for the slice state
 export interface CustomerAtsEDividendState {
     ats: BankInfoModel[],
-    idRowsMain: number,
+    idRowsMainAts: number,
     eDividend: BankInfoModel[],
+    idRowsMainEDividend: number,
 }
 
 // Define the initial state using that type
 const initialState: CustomerAtsEDividendState = {
     ats: [],
-    idRowsMain: -1,
-    eDividend: []
+    idRowsMainAts: -1,
+    eDividend: [],
+    idRowsMainEDividend: -1,
 }
 
 export const customerAtsEDividendSlice = createSlice({
@@ -19,11 +21,14 @@ export const customerAtsEDividendSlice = createSlice({
     // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
-        setIsMainCustomerAts: (state, action: PayloadAction<number>) => {
-            state.idRowsMain = action.payload
+        setMainIdCustomerAts: (state, action: PayloadAction<number>) => {
+            state.idRowsMainAts = action.payload
         },
         setDataCustomerAts(state, action: PayloadAction<BankInfoModel[]>) {
             state.ats = action.payload
+        },
+        setMainIdCustomerEDividend: (state, action: PayloadAction<number>) => {
+            state.idRowsMainEDividend = action.payload
         },
         setDataCustomerEDividend(state, action: PayloadAction<BankInfoModel[]>) {
             state.eDividend = action.payload
@@ -31,6 +36,6 @@ export const customerAtsEDividendSlice = createSlice({
     }
 })
 
-export const { setDataCustomerAts, setDataCustomerEDividend, setIsMainCustomerAts } = customerAtsEDividendSlice.actions
+export const { setDataCustomerAts, setDataCustomerEDividend, setMainIdCustomerAts, setMainIdCustomerEDividend } = customerAtsEDividendSlice.actions
 
 export default customerAtsEDividendSlice.reducer
